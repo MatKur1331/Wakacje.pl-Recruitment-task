ebf6717ec193f541fba209c871398fef9e4a40f2
MatKur1331 <82779261+MatKur1331@users.noreply.github.com>
2022-06-24 12:22:19 +0200

    Update solution1.php

--- solution1.php
+++ solution1.php
@@ -1,28 +1,39 @@
 <?php
+
+function printList($limit,$array,$type){
+    $array = array_count_values($array);
+    arsort($array);
+    echo "Najczęściej występujące $type";
+    echo "<ol>";
+    foreach ($array as $key => $val)
+    {
+            if($limit>0)
+            {
+                echo "<li>$key $val wystąpień</li>";
+                $limit--;
+            }
+            else break;
+    }
+    echo "</ol>";
+}
+
 $file = file('php_internship_data.csv');
 $names = array();
 $dates = array();
+//split dates and names
     foreach($file as $line)
     {
         $line = trim($line);
         $row = explode(",",$line);
-        $line = $row[0];
-        $line = ucfirst(strtolower($line));
-        array_push($names,$line);
-    }
-    $names = array_count_values($names);
-    arsort($names);
-        $limit=10;
-        echo "Najczęściej występujące imiona";
-        echo "<ol>";
-        foreach ($names as $key => $val)
-        {
-                if($limit>0)
-                {
-                    echo "<li>$key $val wystąpień</li>";
-                    $limit--;
-                }
-                else break;
+        $name = ucfirst(strtolower($row[0]));
+        array_push($names,$name);
+        if($row[1]>='2000-01-01')
+        {        
+            $row[1] = date('d.m.Y',strtotime($row[1]));
+            array_push($dates,$row[1]);
         }
-        echo "</ol>";
+    }
+    printList(10,$names,'imiona');
+    printList(10,$dates,'daty');
+
 ?>
